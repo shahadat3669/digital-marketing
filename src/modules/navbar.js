15d854284880c58632416fd36c20f5b5c50a9e3a
Shahadat Hossain <shahadat3669@gmail.com>
2023-01-02 23:48:48 +0600

    Add toggle button functionality in navbar.js module.

--- src/modules/navbar.js
+++ src/modules/navbar.js
@@ -1,3 +1,10 @@
-const navbar = () => {};
+const toggle = document.querySelector('.header__toggle-btn');
+const collapse = document.querySelector('.header__nav-collapse');
+
+const navbar = () => {
+  toggle.addEventListener('click', () => {
+    collapse.classList.toggle('header__nav-active');
+  });
+};
 
 export default navbar;
